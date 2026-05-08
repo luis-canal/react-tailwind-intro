b96b86b5baa3251659242862d792c2f176234462
Luis Eduardo Brescansin Canal <luiseduardocanal@gmail.com>
2026-05-08 23:39:59 +0000

    instanciado Hello no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/global.css'
 import App from './App.jsx'
+import Hello from './Hello.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Hello />
   </StrictMode>,
 )
