229e905e140453d67e58a78114a54f5bb279e9aa
Luis Eduardo Brescansin Canal <luiseduardocanal@gmail.com>
2026-05-09 00:44:23 +0000

    criado um componente Product

---
+++ src/Product.jsx
@@ -0,0 +1,8 @@
+export function Product({title = "Não informado", price = "Consulte"}) {
+    return(
+        <>
+            <h1>(title)</h1>
+            <h2>(price)</h2>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './assets/global.css'
 import App from './App.jsx'
 import Hello from './Hello.jsx'
+import { Product } from './Product.jsx'
+
+const DATA = [
+  {title: "abóbora", price: 55},
+  {title: "banana", price: 321},
+  {title: "abacate", price: 123},
+  {title: "pirulito de pirulito", price: 40000}
+];
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,5 +18,7 @@ createRoot(document.getElementById('root')).render(
     <Hello />
     <Hello name="Luis"/>
     <Hello name="Pedro"/>
+
+    pro
   </StrictMode>,
 )
