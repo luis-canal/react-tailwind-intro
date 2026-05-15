4d3a180970a49a780ff148d37d62b0b5cccda96c
Luis Eduardo Brescansin Canal <luiseduardocanal@gmail.com>
2026-05-15 20:18:13 -0300

    Criado 3 funções dentro do componentes Hello

---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+function UserLogged() {
+    <div>Usuário logado!</div>
+}
+
+function DoLogin(){
+    return <div>Clique aqui e faça login</div>
+}
+
+export function Hello(isLogged = false, username = ""){
+
+}
