dc29ca06ef5ff0187d19b357a8614ec7a58c69b0
Luis Eduardo Brescansin Canal <luiseduardocanal@gmail.com>
2026-05-16 00:52:50 +0000

    Ajustado o código de App.jsx para receber useEffect e controlar os logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Hello } from "./components/Hello"
 import { useState } from "react"
+import { useEffect } from "react"
 
 function App() {
   const [userState, setUserState] = useState(false)
   const [username, setUsername] = useState("Luis")
 
-  setTimeout(() => {setUserState(true)}, 5000)      // Simula um login após 5 segundos
 
-  return <Hello isLogged={userState} username={username} /> 
+  useEffect(() => {console.log("O componente foi montado")}, []);
+  useEffect(() => {console.log("O estado do usuário mudou para: ", userState)}, [userState]);
+
+  //setTimeout(() => {setUserState(true)}, 5000)      // Simula um login após 5 segundos
+
+  return <Hello isLogged={userState} username={username} setUserState={setUserState} />
 }
 
 export default App 
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,10 +2,10 @@ function UserLogged({ username }) {
     return <div>Seja bem-vindo, {username}!</div>
 }
 
-function DoLogin() {
-    return <div>Clique aqui e faça login</div>
+function DoLogin({ setUserState }) {
+    return <button onClick={() => setUserState(true)}>Clique aqui e faça login</button>
 }
 
-export function Hello({ isLogged = false, username = "" }) {
-    return isLogged ? <UserLogged username={username} /> : <DoLogin />
+export function Hello({ setUserState, isLogged = false, username = "" }) {
+    return isLogged ? <UserLogged username={username} /> : <DoLogin setUserState={setUserState} />
 }
