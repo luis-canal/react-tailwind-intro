12f4568536a2e0ef4658b57dea77877b30dd75f0
Luis Eduardo Brescansin Canal <luiseduardocanal@gmail.com>
2026-05-16 00:19:35 +0000

    Alterado a variavel para userState

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Hello } from "./components/Hello"
 import { useState } from "react"
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false)
+  const [userState, setUserState] = useState(false)
   const [username, setUsername] = useState("Luis")
 
-  return <Hello isLogged={isLogged} username={username} />
+  return <Hello isLogged={userState} username={username} />
 }
 
 export default App
